65807b475aa41c4ac27f7c908a0025342d5e4caa
Richard <info@richardmenning.de>
2019-02-09 15:28:14 +0100

    button hinzugefügt

--- docstation/src/App.js
+++ docstation/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import NavBar from './components/NavBar';
-import DocsCalendar from './components/DocsCalendar';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import DocsCalendar from "./components/DocsCalendar";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+import "./App.css";
 
+const styles = {
+  Button: {
+    margin: '20px'
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <DocsCalendar/>
+        <DocsCalendar />
+        <Button variant="contained" color="primary" style={styles.Button}>
+          Termin hinzufügen 
+        </Button>
       </div>
     );
   }
